bd722010d05316f097948d8d195bdc3660cdb2e4
Ethan <ethanjohol@gmail.com>
2022-08-09 14:17:37 +1000

    Add Prod, Staging, Dev (feature) cfn environment creation

--- tasks.py
+++ tasks.py
@@ -1,54 +1,119 @@
-# pylint: disable=redefined-outer-name
+# pylint: disable=redefined-outer-name,line-too-long
 from invoke import task, Collection
 
+global_col = Collection("global")
 prod = Collection("prod")
+staging = Collection("staging")
 dev = Collection("dev")
-namespace = Collection(prod, dev)
+namespace = Collection(global_col, prod, staging, dev)
 
 
 @task
-def clean(ctx):
-    with ctx.cd("infrastructure"):
-        ctx.run(
-            "make clean",
-            env={"ENV_NAME": ctx.ENV_NAME, "AWS_REGION": ctx.AWS_REGION},
-        )
+def build(ctx, template):
+    ctx.run("sam build " f"--template {template} " "--build-dir .aws-sam/build ")
 
 
 @task
-def build(ctx, clean=False):
-    with ctx.cd("infrastructure"):
-        if clean:
-            ctx.run(
-                "make clean",
-                env={"ENV_NAME": ctx.ENV_NAME, "AWS_REGION": ctx.AWS_REGION},
-            )
+def deploy(ctx, feature_name=""):
+    stack_name = f"{ctx.ENV_NAME}"
+    env_name = ctx.ENV_NAME
+    if ctx.COLLECTION_NAME == "dev":
+        if not feature_name:
+            raise AssertionError("dev environment must have feature_name")
+        stack_name += f"-{feature_name}"
+        env_name += f"-{feature_name}"
+        repository_branch = feature_name
+    else:
+        if feature_name:
+            raise AssertionError("Only dev environment can have feature_name")
+        if ctx.COLLECTION_NAME == "prod":
+            repository_branch = "production"
+        elif ctx.COLLECTION_NAME == "staging":
+            repository_branch = "master"
 
-        ctx.run(
-            "make build-template",
-            env={"ENV_NAME": ctx.ENV_NAME, "AWS_REGION": ctx.AWS_REGION},
-        )
+    global_deploy(ctx)
+    build(ctx, "infrastructure/template.yaml")
+    ctx.run(
+        "sam deploy "
+        f"--stack-name {stack_name} "
+        "--s3-bucket aws-sam-$(aws sts get-caller-identity --no-cli-pager --query Account --output text) "
+        f"--s3-prefix {stack_name} "
+        f"--region {ctx.AWS_REGION} "
+        "--no-confirm-changeset "
+        "--no-fail-on-empty-changeset "
+        "--capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND "
+        "--parameter-overrides "
+        f'ParameterKey="EnvironmentName",ParameterValue="{env_name}" '
+        f'ParameterKey="RepositoryBranch",ParameterValue="{repository_branch}" '
+        "--resolve-image-repos "
+    )
 
 
 @task
-def deploy(ctx, clean=False):
-    with ctx.cd("infrastructure"):
-        if clean:
-            ctx.run(
-                "make clean",
-                env={"ENV_NAME": ctx.ENV_NAME, "AWS_REGION": ctx.AWS_REGION},
-            )
+def dependencies_deploy(ctx, feature_name=""):
+    stack_name = f"{ctx.ENV_NAME}-dependencies"
+    env_name = ctx.ENV_NAME
+    if ctx.COLLECTION_NAME == "dev":
+        if not feature_name:
+            raise AssertionError("dev environment must have feature_name")
+        stack_name += f"-{feature_name}"
+        env_name += f"-{feature_name}"
+        repository_branch = feature_name
+    else:
+        if feature_name:
+            raise AssertionError("Only dev environment can have feature_name")
+        if ctx.COLLECTION_NAME == "prod":
+            repository_branch = "production"
+        elif ctx.COLLECTION_NAME == "staging":
+            repository_branch = "master"
 
-        ctx.run(
-            "make deploy-template",
-            env={"ENV_NAME": ctx.ENV_NAME, "AWS_REGION": ctx.AWS_REGION},
-        )
+    global_deploy(ctx)
+    build(ctx, "infrastructure/dependencies-template.yaml")
+    ctx.run(
+        "sam deploy "
+        f"--stack-name {stack_name} "
+        "--s3-bucket aws-sam-$(aws sts get-caller-identity --no-cli-pager --query Account --output text) "
+        f"--s3-prefix {stack_name} "
+        f"--region {ctx.AWS_REGION} "
+        "--no-confirm-changeset "
+        "--no-fail-on-empty-changeset "
+        "--capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND "
+        "--parameter-overrides "
+        f'ParameterKey="EnvironmentName",ParameterValue="{env_name}" '
+        f'ParameterKey="RepositoryBranch",ParameterValue="{repository_branch}" '
+        "--resolve-image-repos "
+    )
 
 
-for invoke_task in (build, deploy, clean):
+@task
+def global_deploy(ctx):
+    build(ctx, "infrastructure/global-template.yaml")
+    ctx.run(
+        "sam deploy "
+        f"--stack-name aw2-global "
+        "--s3-bucket aws-sam-$(aws sts get-caller-identity --no-cli-pager --query Account --output text) "
+        f"--s3-prefix aw2-global "
+        f"--region {ctx.AWS_REGION} "
+        "--no-confirm-changeset "
+        "--no-fail-on-empty-changeset "
+        "--capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND "
+        "--resolve-image-repos "
+    )
+
+
+@task
+def create_env(ctx, feature_name=""):
+    dependencies_deploy(ctx, feature_name=feature_name)
+
+
+for invoke_task in (build, deploy, dependencies_deploy, create_env):
     prod.add_task(invoke_task)
+    staging.add_task(invoke_task)
     dev.add_task(invoke_task)
+global_col.add_task(global_deploy)
 
-prod.configure({"ENV_NAME": "aw2-prod"})
-dev.configure({"ENV_NAME": "aw2-dev"})
+global_col.configure({"COLLECTION_NAME": "global", "ENV_NAME": "aw2-global"})
+prod.configure({"COLLECTION_NAME": "prod", "ENV_NAME": "aw2-prod"})
+staging.configure({"COLLECTION_NAME": "staging", "ENV_NAME": "aw2-staging"})
+dev.configure({"COLLECTION_NAME": "dev", "ENV_NAME": "aw2-dev"})
 namespace.configure({"AWS_REGION": "ap-southeast-2"})
